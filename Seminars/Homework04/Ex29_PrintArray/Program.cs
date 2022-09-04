70d219a5bd04d3d8fe6801c170580e8746368a39
nikvladimir <alfa.voland@mail.ru>
2022-09-04 16:16:21 +0300

    Homework04: code review

--- Seminars/Homework04/Ex29_PrintArray/Program.cs
+++ Seminars/Homework04/Ex29_PrintArray/Program.cs
@@ -1,14 +1,14 @@
-﻿// Задача 23
+﻿// Задача 29
 // Напишите программу, которая задаёт массив из 8 элементов 
 // и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
 
-double[] CreateArray()
+double[] CreateArray(int length)
 {
   double[] array = new double[] { };
-  for (int i = 1; i <= 8; i++)
+  for (int i = 1; i <= length; i++)
   {
     array = array.Append(new Random().Next(0, 10)).ToArray();
   }
@@ -16,9 +16,10 @@ double[] CreateArray()
 }
 
 
-void PrintArray(double[] array)
+void PrintArray<T>(T[] array)
 {
-  Console.WriteLine(string.Join(", ", array));
+  Console.WriteLine($"[{string.Join(", ", array)}]");
 }
 
-PrintArray(CreateArray());
+PrintArray(CreateArray(8));
+PrintArray(CreateArray(3));
