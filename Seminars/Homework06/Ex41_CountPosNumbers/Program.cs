5e51ad85042b41c00dc0a8e61cce35d70172b9ba
nikvladimir <alfa.voland@mail.ru>
2022-09-05 23:19:33 +0300

    Homework_6: ex41 ex43

---
+++ Seminars/Homework06/Ex41_CountPosNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 4
+
+string text1 = "0, 7, 8, -2, -2";
+string text2 = "1, -7, 567, 89, 223";
+
+int CountPosNumbers(string array)
+{
+  var posNums = array.Split(", ")
+                  .Select(int.Parse)
+                  .Where(e => e > 0)
+                  .ToArray();
+  return posNums.Length;
+}
+
+System.Console.WriteLine(CountPosNumbers(text1));
+System.Console.WriteLine(CountPosNumbers(text2));
---
+++ Seminars/Homework06/Ex43_PointOfIntersection/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double[] PointOfIntersection(int b1, int k1, int b2, int k2)
+{
+    double x = ((double)b2 - (double)b1) / ( (double)k1 - (double)k2);
+    double y = ((double)k1 * x) + (double)b1;
+    return new double[] { x, y };
+}
+
+void PrintArray<T>(T[] array)
+{
+  Console.WriteLine($"[{string.Join("; ", array)}]");
+}
+
+PrintArray(PointOfIntersection(2, 5, 4, 9));
